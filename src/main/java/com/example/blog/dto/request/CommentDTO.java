b6f09cd0ffe2a05b3b1ab4d1ed20018fcf64157a
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2022-04-19 09:10:45 -0300

    removing unused import

--- src/main/java/com/example/blog/dto/request/CommentDTO.java
+++ src/main/java/com/example/blog/dto/request/CommentDTO.java
@@ -5,8 +5,6 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
