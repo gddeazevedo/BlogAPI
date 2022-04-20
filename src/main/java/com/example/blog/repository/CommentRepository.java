8e699c5dce75bb1a4677b22d2814540fc635802a
gddeazevedo <gabrielcapuerj@gmail.com>
2022-04-20 13:09:53 -0300

    Removing unused imports

--- src/main/java/com/example/blog/repository/CommentRepository.java
+++ src/main/java/com/example/blog/repository/CommentRepository.java
@@ -1,12 +1,8 @@
 package com.example.blog.repository;
 
-import java.util.List;
-
 import com.example.blog.entity.Comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
